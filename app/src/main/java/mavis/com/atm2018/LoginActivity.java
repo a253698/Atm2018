462494a862989083900ebeda725d1a31502c40ca
Mavis <angel253698@gmail.com>
2018-11-13 15:42:15 +0800

    getSharedPreferences

--- app/src/main/java/mavis/com/atm2018/LoginActivity.java
+++ app/src/main/java/mavis/com/atm2018/LoginActivity.java
@@ -6,19 +6,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText edusername;
+    private EditText edpassword;
+    private String username;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findView();
+//        edusername = findViewById(R.id.ed_userName);
+        String username = getSharedPreferences("atm", MODE_PRIVATE).getString("USERNAME" , " ");
+        edusername.setText(username);
+        //用空字串，第一次登入才不會產生null
     }
     public void login(View view) {
-        EditText edusername = findViewById(R.id.ed_userName);
-        EditText edpassword = findViewById(R.id.ed_password);
-        String username = edusername.getText().toString();
-        String password = edpassword.getText().toString();
+        findView();
+        username = edusername.getText().toString();
+        password = edpassword.getText().toString();
         if ("mavis".equals(username) && "253698".equals(password)) {
 //            SharedPreferences settings = getSharedPreferences("atm", MODE_PRIVATE);
 //            settings.edit().putString().apply();
@@ -30,8 +41,17 @@ public class LoginActivity extends AppCompatActivity {
             setResult(RESULT_OK);
             finish();
         }else{
-            new AlertDialog.Builder(this).setMessage("Wrong user name or wrong password.").setTitle("ERROR!").setPositiveButton("OK!", null).show();
+            new AlertDialog.Builder(this)
+                    .setMessage("Wrong user name or wrong password.")
+                    .setTitle("ERROR!")
+                    .setPositiveButton("OK!", null)
+                    .show();
 
         }
+    }
+
+    private void findView() {
+        edusername = findViewById(R.id.ed_userName);
+        edpassword = findViewById(R.id.ed_password);
     }
 }
