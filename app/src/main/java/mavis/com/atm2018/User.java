df737e306729082fd17beb3b2fd967fac1ed5c51
Mavis <angel253698@gmail.com>
2018-11-20 16:03:55 +0800

    User

--- app/src/main/java/mavis/com/atm2018/User.java
+++ app/src/main/java/mavis/com/atm2018/User.java
@@ -12,6 +12,8 @@ public class User {
     String gender ;
     int age ;
     SharedPreferences setting ;
+    boolean userinfo = false;
+
 
     public User(Context context){
         this.context = context;
@@ -27,28 +29,44 @@ public class User {
     }
 
     public String getNickname() {
+        if( nickname == null){
+            nickname = setting.getString("NICKNAME", null);
+        }
         return nickname;
     }
 
     public void setNickname(String nickname) {
+        setting.edit().putString("NICKNAME" , nickname).apply();
         this.nickname = nickname;
     }
 
     public String getGender() {
+        if ( gender == null){
+            gender = setting.getString("GENDER" , null);
+        }
         return gender;
     }
 
     public void setGender(String gender) {
+        setting.edit().putString("GENDER" , gender).apply();
         this.gender = gender;
     }
 
     public int getAge() {
+        if (age == 0){
+            age = setting.getInt("AGE" , 0);
+        }
         return age;
     }
 
     public void setAge(int age) {
+        setting.edit().putInt("AGE" , age).apply();
         this.age = age;
     }
 
+    public boolean isUserinfo() {
+        userinfo = ( getNickname() != null && getGender() != null && getAge() != 0 );
+        return userinfo;
+    }
 
 }
